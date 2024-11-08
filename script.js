00cdd6b731bd28fc4c13945d1fb30fb30736fbb0
jooedvardtanar <jooedvardtanar@gmail.com>
2024-11-08 20:04:02 +0100

    dolgozat_forras: minify

--- script.js
+++ script.js
@@ -1,15 +1,1 @@
-document.addEventListener('contextmenu', event => event.preventDefault());
-document.onkeydown = function (e) {
-    if (event.keyCode == 123) {
-        return false;
-    }
-    if (e.ctrlKey && e.shiftKey && e.keyCode == 'I'.charCodeAt(0)) {
-        return false;
-    }
-    if (e.ctrlKey && e.shiftKey && e.keyCode == 'J'.charCodeAt(0)) {
-        return false;
-    }
-    if (e.ctrlKey && e.keyCode == 'U'.charCodeAt(0)) {
-        return false;
-    }
-} 
+document.addEventListener("contextmenu",(e=>e.preventDefault())),document.onkeydown=function(e){return 123!=event.keyCode&&((!e.ctrlKey||!e.shiftKey||e.keyCode!="I".charCodeAt(0))&&((!e.ctrlKey||!e.shiftKey||e.keyCode!="J".charCodeAt(0))&&((!e.ctrlKey||e.keyCode!="U".charCodeAt(0))&&void 0)))};
